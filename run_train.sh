f66433240700c2b5acc69ab4a8e58222d3a6bee1
Gaurang M <138219416+gaurang-mahajan@users.noreply.github.com>
2025-02-15 00:12:39 +0530

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -3,3 +3,5 @@
 python ./train.py --model-type=scgpt --num-epochs=20 --dataset=norman --device=cuda:0 --batch-size=16 --eval-batch-size=16
 
 python ./train.py --model-type=scgenept_go_all_gpt_concat --num-epochs=20 --dataset=norman --device=cuda:0 --batch-size=16 --eval-batch-size=16
+
+python ./train.py --model-type=scgpt_node2vec --num-epochs=20 --dataset=norman --device=cuda:0 --batch-size=16 --eval-batch-size=16
